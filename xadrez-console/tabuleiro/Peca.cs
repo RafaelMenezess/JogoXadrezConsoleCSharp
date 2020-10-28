d7a03dc311daae82ae9542595da66a3f9a57ac44
Rafael Menezes <rafa.rcm@hotmail.com>
2020-10-27 21:25:19 -0300

    Comecando a implementar o metodo que imprime o tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -6,7 +6,7 @@ namespace tabuleiro
         public Posicao posicao { get; set; }
         public Cor cor { get; set; }
         public int qteMovimentos { get; set; }
-        public Tabuleiro tab { get; protected set; } 
+        public Tabuleiro tab { get; protected set; }
 
         public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
         {
